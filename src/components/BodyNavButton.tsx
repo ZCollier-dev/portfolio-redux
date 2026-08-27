58526d7b2de25f39dea5d42e4007a30780e6351f
Zachary Collier <zacharycollier36@protonmail.com>
2026-08-27 18:00:03 -0230

    - Added buttons for body navigation - Added body buttons to home page - Prettied up how all buttons are laid out - Tweaked font sizes a bit

---
+++ src/components/BodyNavButton.tsx
@@ -0,0 +1,7 @@
+import "../styles/BodyNavButton.css"
+
+export default function BodyNavButton(props: {name: string, link: string}) {
+  return <a className="body-button" href={props.link}>
+    <button>{props.name}</button>
+  </a>
+}
--- src/components/HeadNavButton.tsx
+++ src/components/HeadNavButton.tsx
@@ -1,7 +1,7 @@
 import "../styles/HeadNavButton.css"
 
 export default function HeadNavButton(props: {name: string, link: string}) {
-  return <a href={props.link}>
+  return <a className="head-button" href={props.link}>
     <button>{props.name}</button>
   </a>
 }
